62ff45469be72524df181fe8070ad6e32fe022de
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-21 17:45:21 +0200

    initial schema setup

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
 
 const app = express();
 
 app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  }),
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    }),
 );
 
-app.listen(4000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
---
+++ schema.js
@@ -0,0 +1,13 @@
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean } = require('graphql');
+
+const LaunchType = new GraphQLObjectType({
+    name: 'Launch',
+    fields: () => ({
+        flight_number: { type: GraphQLInt },
+        mission_name: { type: GraphQLString },
+        launch_year: { type: GraphQLString },
+        launch_date_local: { type: GraphQLString },
+        launch_success: { type: GraphQLBoolean },
+        rocket: { type: RocketType }
+    })
+});

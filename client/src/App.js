05686c5f5fc8bd005dbcfaaf7cb91290b1f4ebcc
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-07-22 09:32:44 +0200

    apollo set up in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import logo from './logo.jpg';
+
+const client = new ApolloClient({
+    uri: 'https://localhost:5000/graphql'
+});
 
 const App = () => {
     return (
-        <div className="App">
-            <img src={logo} alt="SpaceX" style={styles.image} />
-        </div>
+        <ApolloProvider client={client}>
+            <div className="container">
+                <img src={logo} alt="SpaceX" style={styles.image} />
+            </div>
+        </ApolloProvider>
     );
-}
+};
 
 const styles = {
     image: {
